0841a94c1a457241eef0377c52c3644b1e1b8684
2300033467Dheeraj <2300033467@kluniversity.in>
2026-06-09 20:23:16 +0530

    Minimum and maximum

--- Trees/Min-Max.py
+++ Trees/Min-Max.py
@@ -3,7 +3,6 @@ class Node:
         self.data = data
         self.right = None
         self.left = None
-
 def insert(root,val):
     if root is None:
         return Node(val)
@@ -13,7 +12,6 @@ def insert(root,val):
         root.right = insert(root.right, val)
     
     return root
-
 def search(root,key):
     if root is None:
         return False
@@ -25,35 +23,35 @@ def search(root,key):
         return search (root.left,key)
     
     return search(root.right,key)
-
 def inorder(root):
     if root is None:
         return
     inorder(root.left)
     print(root.data,end=" ")
     inorder(root.right)
-
 def display(root):
     if root is None:
         return
     print(root.data, end =" ")
     display(root.left)
     display(root.right)
-    
 
+def minimum(root):
+    while root.left:
+        root = root.left    
+    return root.data
+
+def maximum(root):
+    while root.right:
+        root = root.right
+    return root.data
 n = int(input())
-
 arr = list(map(int, input().split()))
-key = int(input("Search:"))
 
 root = None
 
 for x in arr:
     root = insert(root, x)
 
-display(root)
-
-if search(root, key):
-    print("Found")
-else:
-    print("Not Found")
+print(minimum(root))
+print(maximum(root))
